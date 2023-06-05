b67a267609a0ad2305392a7d82acb7286ea84c21
Elizabeth Yang <eyang@Elizabeths-MacBook-Pro.local>
2023-06-05 12:49:37 -0400

    Prepare covtype67 binary rff experiments

--- multi-dim-featcreation/covtype67_all_experiments/covtype67_privaterff_binary.py
+++ multi-dim-featcreation/covtype67_all_experiments/covtype67_privaterff_binary.py
@@ -1,9 +1,14 @@
+from sklearnex import patch_sklearn 
+patch_sklearn()
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from statistics import median
 from math import log
 
+import sys
+sys.path.append('../')
 from dp_sketch import DP_Join
 
 from sklearn import metrics 
@@ -15,13 +20,17 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
-method_to_obj = {'Naive Bayes': GaussianNB(),
-				'Decision Tree': DecisionTreeClassifier(),
-				'Logistic Regression': LogisticRegression(),
+method_to_obj = {'NaiveBayes': GaussianNB(),
+				'DecisionTree': DecisionTreeClassifier(),
+				'LogisticRegression': LogisticRegression(),
 				'SVM': SVC(),
 				'AdaBoost': AdaBoostClassifier(), 
-				'Random Forest': RandomForestClassifier()}
+				'RandomForest': RandomForestClassifier(n_jobs = 2),
+				'MultiLayerPerceptron': MLPClassifier(),
+				'KNN': KNeighborsClassifier(n_jobs = 2)}
 
 def prep_data(file, l_name, index_name = None, f_names = None, test_size = 0.2, center_data = False):
 	# Load dataset 
@@ -53,24 +62,24 @@ def rand_round(mat):
 	thres = round_threshold_vec(mat)
 	rand_mat = np.random.uniform(size = (mat.shape[0], mat.shape[1]))
 	binary_mat = rand_mat > thres 
-	return (binary_mat * 2 * 2 ** (0.5)) - 2 ** (0.5)
+	return (binary_mat * 2 * (2 ** (0.5))) - 2 ** (0.5)
 
 def get_rffs(mat, dim, bandwidth):
 	omega = (2 ** (0.5)) * np.random.normal(0, 1, size = (mat.shape[1], dim)) / (bandwidth ** 2)
 	beta = np.random.uniform(0, 2 * np.pi, dim).reshape(1, -1)
 	return omega, beta, (2 ** (0.5)) * np.cos(np.matmul(mat, omega) + beta)
 
-def get_loss(f_train, l_train, f_test, l_test, alg = 'Logistic Regression'):
+def get_loss(f_train, l_train, f_test, l_test, alg = 'LogisticRegression'):
 	classifier = method_to_obj[alg]
-	classifier.fit(f_train, l_train.replace(-1, 0).to_numpy().reshape(l_train.size))
+	classifier.fit(f_train, l_train.to_numpy().reshape(l_train.size))
 	pred = classifier.predict(f_test)
-	return metrics.accuracy_score(l_test.replace(-1, 0).to_numpy().reshape(l_test.size), pred)
+	return metrics.accuracy_score(l_test.to_numpy().reshape(l_test.size), pred)
 
 if __name__ == "__main__":
 	num_trials = 25
 	bandwidth = 40
 
-	file = '../data/covtype.csv'
+	file = '../../data/covtype.csv'
 	l_name = ['Cover_Type']
 	f_train, l_train, f_test, l_test = prep_data(file, l_name)
 	f_names = f_train.columns
@@ -84,9 +93,9 @@ if __name__ == "__main__":
 	print(l_test.value_counts())
 	
 	f_test, l_test = f_test[f_names], l_test[l_name].loc[f_test.index]
-	l_train = l_train.replace(6, -1)
+	l_train = l_train.replace(6, 0)
 	l_train = l_train.replace(7, 1)
-	l_test = l_test.replace(6, -1)
+	l_test = l_test.replace(6, 0)
 	l_test = l_test.replace(7, 1)
 
 	index_train = f_train.index
@@ -95,7 +104,7 @@ if __name__ == "__main__":
 
 	sketch_dim = [5, 10, 15, 20, 25]
 	total_eps_list = [1.0, 2.0, 3.0, 4.0, 5.0]
-	algs = ['Logistic Regression', 'AdaBoost']
+	algs = ['RandomForest', 'KNN']
 
 	trial_dict = {}
 	loss_dict = {}
@@ -111,6 +120,7 @@ if __name__ == "__main__":
 			loss_dict[alg]['Eps = %s 25' % str(total_eps)] = []
 			loss_dict[alg]['Eps = %s 75' % str(total_eps)] = []
 		loss_ctrl[alg] = get_loss(f_train, l_train, f_test, l_test, alg)
+		loss_dict[alg]['Original Features'] = []
 	print(loss_ctrl)
 	
 	for dim in sketch_dim:
@@ -136,21 +146,21 @@ if __name__ == "__main__":
 				trial_dict[alg]['RFF Binary'].append(get_loss(f_train_rff, l_train, f_test_rff, l_test, alg))
 
 			f_train_rff = pd.DataFrame(data = f_train_rff, index = index_train, columns = ["Feat %i" % (i + 1) for i in range(dim)])
-			sens_list = [2 ** (0.5) for i in range(len(f_names))]
+			sens_list = [2 ** (0.5) for i in range(dim)]
 			for total_eps in total_eps_list:
 				print('Total Eps = %s' % str(total_eps))
 				eps_memb = 10000 # total_eps / (dim + 1)
 				eps_val = total_eps # - eps_memb
 
-				dp_join = DP_Join(eps_memb, eps_val, sens_list) # TODO: EDIT
-				dp_join.join(l_train, f_train_rff, 'Real') # TODO: EDIT
-				dp_join.flip_labels(l_name[0])
+				dp_join = DP_Join(eps_memb, eps_val, sens_list) 
+				dp_join.join(l_train, f_train_rff, 'Binary') 
 
 				for alg in algs:
-					trial_dict[alg]['Eps = %s' % total_eps].append(get_loss(dp_join.df[f_train_rff.columns].to_numpy(), dp_join.df[l_train.columns], f_test_rff, l_test, alg))
+					trial_dict[alg]['Eps = %s' % total_eps].append(get_loss(dp_join.features, dp_join.labels, f_test_rff, l_test, alg))
 
 		for alg in algs:
 			loss_dict[alg]['Dimension'].append(dim)
+			loss_dict[alg]['Original Features'].append(loss_ctrl[alg])
 			loss_dict[alg]['RFF Binary'].append(median(trial_dict[alg]['RFF Binary']))
 			loss_dict[alg]['RFF Binary 25'].append(median(trial_dict[alg]['RFF Binary']) - np.percentile(trial_dict[alg]['RFF Binary'], 25))
 			loss_dict[alg]['RFF Binary 75'].append(np.percentile(trial_dict[alg]['RFF Binary'], 75) - median(trial_dict[alg]['RFF Binary']))
@@ -166,19 +176,22 @@ if __name__ == "__main__":
 		alg_df = alg_df / loss_ctrl[alg]
 		print(alg_df)
 
-		file = 'covtype_rffbinarylaplace_%s_limit_trials=%i' % (alg.lower(), num_trials)
+		file = 'covtype_rffbinary_%s_limit_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
-		shift = -0.5
+		shift = -0.25
+		plt.ylim((0.0, 1.0))
+		plt.errorbar(alg_df.index + shift, alg_df['Original Features'], \
+			yerr = np.zeros(shape = (2, len(alg_df))), label = 'Original Features')
 		plt.errorbar(alg_df.index + shift, alg_df['RFF Binary'], \
 			yerr = alg_df[['RFF Binary 25', 'RFF Binary 75']].to_numpy().T, label = 'RFF Binary')
-		shift += 0.1
+		shift += 0.05
 		for total_eps in total_eps_list:
 			plt.errorbar(alg_df.index + shift, alg_df['Eps = %s' % str(total_eps)], \
 				yerr = alg_df[['Eps = %s 25' % str(total_eps), 'Eps = %s 75' % str(total_eps)]].to_numpy().T, label = 'Eps = %s' % str(total_eps))
-			shift += 0.1
+			shift += 0.05
 
 		plt.xlabel("Dimension")
-		plt.ylabel("(Loss With Dim Reduction) / (Actual Loss)")
+		plt.ylabel("Accuracy")
 		plt.legend(loc = "lower right")
 		plt.savefig('%s.jpg' % file)
 		plt.close()
