1f584598fe156e9febea6c6aea3eb9aa6143b0bb
Elizabeth Yang <eyang@Elizabeths-MacBook-Pro.local>
2023-06-05 23:25:37 -0400

    Set up scripts for MNIST pca test

--- multi-dim-featcreation/mnist49_all_experiments/mnist49_privatepca.py
+++ multi-dim-featcreation/mnist49_all_experiments/mnist49_privatepca.py
@@ -170,7 +170,7 @@ if __name__ == "__main__":
 		alg_df = alg_df
 		print(alg_df)
 
-		file = 'covtype_pca_%s_smalleps_highdim_trials=%i' % (alg.lower(), num_trials)
+		file = 'mnist49_pca_%s_smalleps_highdim_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
