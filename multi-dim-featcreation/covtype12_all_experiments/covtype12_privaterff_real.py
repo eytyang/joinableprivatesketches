a1a834953464ab7772bd9a6352f54a20367d231c
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-06-17 08:29:26 -0700

    Modify covtype12 rff script

--- multi-dim-featcreation/covtype12_all_experiments/covtype12_privaterff_real.py
+++ multi-dim-featcreation/covtype12_all_experiments/covtype12_privaterff_real.py
@@ -99,14 +99,14 @@ if __name__ == "__main__":
 	f_test = f_test.to_numpy()
 
 	# Compute bandwidth
-	bandwidth_list = []
-	for i in range(10):
-		rand_rows = np.random.choice(f_train.shape[0], 10000)
-		pair_dists = sc.spatial.distance.pdist(f_train[rand_rows, :])
-		bandwidth_list.append(np.median(pair_dists))
-	bandwidth = sum(bandwidth_list) / 10
-	print(bandwidth)
-
+	# bandwidth_list = []
+	# for i in range(10):
+	# 	rand_rows = np.random.choice(f_train.shape[0], 10000)
+	# 	pair_dists = sc.spatial.distance.pdist(f_train[rand_rows, :])
+	# 	bandwidth_list.append(np.median(pair_dists))
+	# bandwidth = sum(bandwidth_list) / 10
+	bandwidth = 2300
+	
 	sketch_dim = [5, 10, 15, 20, 25]
 	total_eps_list = [1.0, 2.0, 3.0, 4.0, 5.0]
 	algs = ['AdaBoost', 'LogisticRegression', 'KNN', 'RandomForest']
