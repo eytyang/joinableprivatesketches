e91622399eaf67d6963765ef7d32ea990f34e02d
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-06-24 16:53:14 -0700

    Modify mnist01 rff experiments

--- multi-dim-featcreation/mnist01_all_experiments/mnist01_privaterff_real.py
+++ multi-dim-featcreation/mnist01_all_experiments/mnist01_privaterff_real.py
@@ -122,17 +122,10 @@ if __name__ == "__main__":
 	print("l_train shape:", l_train.shape)
 	print("l_test shape:", l_test.shape)
 
-	sketch_dim = [10, 20, 30, 40, 50]
+	sketch_dim = [5, 10, 15, 20, 25]
 	total_eps_list = [1.0, 2.0, 3.0, 4.0, 5.0]
-<<<<<<< HEAD
 	algs = ['MultiLayerPerceptron']
-=======
-	algs = ['AdaBoost', 'LogisticRegression', 'MultiLayerPerceptron']
-	# algs = ['KNN', 'RandomForest']
-<<<<<<< HEAD
->>>>>>> b4670b0 (Run epilepsy PCA experiments)
-=======
->>>>>>> b4670b0 (Run epilepsy PCA experiments)
+	# algs = ['AdaBoost', 'LogisticRegression', 'MultiLayerPerceptron']
 
 	trial_dict = {}
 	loss_dict = {}
@@ -200,7 +193,7 @@ if __name__ == "__main__":
 		alg_df = alg_df 
 		print(alg_df)
 
-		file = 'mnist01_rffrealclip_%s_trials=%i' % (alg.lower(), num_trials)
+		file = 'mnist01_rffrealclip_smalldim_%s_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
