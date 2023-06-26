dafd6ed838973b330e773457d3d013b28cf0582c
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-06-26 10:57:27 -0700

    Attempt clipping

--- multi-dim-featcreation/covtype67_all_experiments/covtype67_privatepca.py
+++ multi-dim-featcreation/covtype67_all_experiments/covtype67_privatepca.py
@@ -160,10 +160,16 @@ if __name__ == "__main__":
 				f_train_priv = np.matmul(f_train, priv_pca)
 				f_test_priv = np.matmul(f_test, priv_pca)
 
+				perc05 = np.percentile(f_train_priv, 5, axis = 0 , keepdims = True)
+				perc95 = np.percentile(f_train_priv, 95, axis = 0 , keepdims = True)
+				f_train_priv = np.clip(f_train_priv, a_min = perc05, a_max = perc95)
 				sens_list = get_sens_list(f_train_priv)
 				f_train_priv = pd.DataFrame(data = f_train_priv, index = index_train, columns = ["Comp %i" % (i + 1) for i in range(dim)])
+				
 				dp_join = DP_Join(eps_memb, eps_val, sens_list, 'Real')
 				dp_join.join(l_train, f_train_priv)
+				dp_join.features = np.clip(dp_join.features, a_min = perc05, a_max = perc95)
+				f_test_priv = np.clip(f_test_priv, a_min = perc05, a_max = perc95)
 
 				for alg in algs:
 					trial_dict[alg].append(get_loss(dp_join.features, dp_join.labels, f_test_priv, l_test, alg))
@@ -180,7 +186,7 @@ if __name__ == "__main__":
 		alg_df = alg_df
 		print(alg_df)
 
-		file = 'covtype67_pca_%s_trials=%i' % (alg.lower(), num_trials)
+		file = 'covtype67_pca_centeredclip90_%s_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
