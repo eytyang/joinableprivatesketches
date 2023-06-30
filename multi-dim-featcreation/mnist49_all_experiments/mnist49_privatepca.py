034289c8992b8467db14b80e360ec2291033c5c3
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-06-29 17:57:59 -0700

    prepare mnist pca script for clipping

--- multi-dim-featcreation/mnist49_all_experiments/mnist49_privatepca.py
+++ multi-dim-featcreation/mnist49_all_experiments/mnist49_privatepca.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
 		alg_df = alg_df
 		print(alg_df)
 
-		file = 'mnist49_pca_centered_%s_trials=%i' % (alg.lower(), num_trials)
+		file = 'mnist49_pca_centeredclip_%s_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
