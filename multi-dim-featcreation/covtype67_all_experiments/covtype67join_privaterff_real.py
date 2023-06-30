7d86ede6f0e4331ea9a025ba5ac8307cccffdd28
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-06-30 15:57:04 -0700

    Add covtype and epileptic perfmemb experiments

--- multi-dim-featcreation/covtype67_all_experiments/covtype67join_privaterff_real.py
+++ multi-dim-featcreation/covtype67_all_experiments/covtype67join_privaterff_real.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
 		alg_df = alg_df 
 		print(alg_df)
 
-		file = 'covtype67join_rffrealclip_%s_trials=%i' % (alg.lower(), num_trials)
+		file = 'covtype67joinperfectmemb_rffrealclip_%s_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
