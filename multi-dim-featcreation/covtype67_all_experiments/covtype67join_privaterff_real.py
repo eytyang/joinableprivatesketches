0bfd6fbf801c38359eba5053b5d0587791ba5a31
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-07-07 16:06:21 -0700

    add epilepsy and covtype experiments

--- multi-dim-featcreation/covtype67_all_experiments/covtype67join_privaterff_real.py
+++ multi-dim-featcreation/covtype67_all_experiments/covtype67join_privaterff_real.py
@@ -121,8 +121,8 @@ if __name__ == "__main__":
 	# bandwidth = np.median(pair_dists)
 	bandwidth = 1500
 
-	sketch_dim = [5, 10, 15, 20, 25]
-	total_eps_list = [1.0, 2.0, 3.0, 4.0, 5.0]
+	sketch_dim = [1]
+	total_eps_list = [1.0, 3.0, 5.0]
 	algs = ['AdaBoost', 'KNN', 'RandomForest']
 
 	trial_dict = {}
@@ -192,7 +192,7 @@ if __name__ == "__main__":
 		alg_df = alg_df 
 		print(alg_df)
 
-		file = 'covtype67joinperfectmemb_rffrealclip_%s_trials=%i' % (alg.lower(), num_trials)
+		file = 'covtype67joinperfectmemb_rffrealclip_dim1_%s_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
