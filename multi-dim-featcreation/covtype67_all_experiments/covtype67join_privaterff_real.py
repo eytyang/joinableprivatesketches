2b8bbf3ecf9abd1cea239f34a0e6faa29e74f4d8
Elizabeth Yang <43188916+eytyang@users.noreply.github.com>
2023-07-07 16:13:13 -0700

    add covertype experiments

--- multi-dim-featcreation/covtype67_all_experiments/covtype67join_privaterff_real.py
+++ multi-dim-featcreation/covtype67_all_experiments/covtype67join_privaterff_real.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
 			sens_list = [2 ** (0.5) for i in range(dim)]
 			for total_eps in total_eps_list:
 				print('Total Eps = %s' % str(total_eps))
-				eps_memb = 1000 # total_eps / (dim + 1)
+				eps_memb = total_eps / (dim + 1)
 				eps_val = total_eps - total_eps / (dim + 1)
 
 				dp_join = DP_Join(eps_memb, eps_val, sens_list) 
@@ -192,7 +192,7 @@ if __name__ == "__main__":
 		alg_df = alg_df 
 		print(alg_df)
 
-		file = 'covtype67joinperfectmemb_rffrealclip_dim1_%s_trials=%i' % (alg.lower(), num_trials)
+		file = 'covtype67join_rffrealclip_dim1_%s_trials=%i' % (alg.lower(), num_trials)
 		alg_df.to_csv('%s.csv' % file)
 		shift = -0.25
 		plt.ylim((0.0, 1.0))
